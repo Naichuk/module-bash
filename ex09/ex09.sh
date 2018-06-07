d53cc267c6b32ae3f3e13e353cf3b9ecb1bc035b
naichukMS <naichuk.maks@gmail.com>
2018-06-07 17:19:57 +0300

    ex09 done

---
+++ ex09/ex09.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+arg=$1
+case "$arg" in
+  "-u"| "--url")
+    grep -o -E "https:\/\/([[:alnum:]]|\.|\/)+" $2
+    ;;
+  "-e" | "--email")
+    grep -o -E  "[[:alnum:]]+\@[[:alpha:]]+\.[[:alpha:]]+" $2
+    ;;
+esac
